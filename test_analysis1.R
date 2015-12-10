dd8f3674fb83798c73cc21f84b48f87f8b3e65bb
U-PaulThinkpad\Paul <Paul@PaulThinkpad.(none)>
2015-12-10 14:49:48 +0530

    dec10 updated

--- test_analysis1.R
+++ test_analysis1.R
@@ -1,7 +1,5 @@
-# create this R file or manually read the required data sets. 
-source("readdata.R") 
 
-source("functions1.R") ; source("functions2.R")
+source("readdata.R") ; source("functions1.R") ; source("functions2.R")
 
 # remove few features
 features_selected <- features_all[-c(38,51,52),]
@@ -14,7 +12,7 @@ train_result <- temp_cv$trainResults_rawData
 test_result  <- temp_cv$testResults_rawData
 
 # run for CV complication with PCA
-proc_data <- gen_proc_data_PCA(data_raw,features_all,data_raw$cv_comp_new)
+proc_data <- gen_proc_data_PCA(data_raw,features_selected,data_raw$cv_comp_new)
 temp_cv <- run_gams_model_PCA(clean_data_pca = proc_data,outcome = data_raw$cv_comp_new,variance_prop = 0.9)
 model <- temp_cv$model 
 train_result <- temp_cv$trainResults_rawData
